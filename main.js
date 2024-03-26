0f33864d8d43f0bc9ec55f1dd079133b72e91667
Mikel3333 <164971500+Mikel3333@users.noreply.github.com>
2024-03-26 10:36:15 +0700

    main.js

--- main.js
+++ main.js
@@ -1,46 +1,13 @@
-var layar = document.getElementById("layar");
-var angkaSebelumnya = null;
-var operator = null;
-
-function tambahAngka(angka) {
-  layar.value += angka;
+function addToDisplay(value) {
+        document.getElementById('display').value += value;
 }
 
-function operasi(op) {
-  if (angkaSebelumnya === null) {
-    angkaSebelumnya = layar.value;
-  } else {
-    hitung();
-  }
-  operator = op;
-  layar.value = "";
-}
+function clearDisplay() {
+        document.getElementById('display').value = '';
+    }
 
-function hitung() {
-  var angkaSekarang = layar.value;
-  var hasil = 0;
-  switch (operator) {
-    case "+":
-      hasil = parseFloat(angkaSebelumnya) + parseFloat(angkaSekarang);
-      break;
-    case "-":
-      hasil = parseFloat(angkaSebelumnya) - parseFloat(angkaSekarang);
-      break;
-    case "*":
-      hasil = parseFloat(angkaSebelumnya) * parseFloat(angkaSekarang);
-      break;
-    case "/":
-      hasil = parseFloat(angkaSebelumnya) / parseFloat(angkaSekarang);
-      break;
-  }
-  layar.value = hasil;
-  angkaSebelumnya = null;
-  operator = null;
-}
-
-function hapus() {
-  layar.value = "";
-  angkaSebelumnya = null;
-  operator = null;
-        }
-    
+function calculate() {
+        var display = document.getElementById('display');
+        var result = eval(display.value);
+        display.value = result;
+    }
